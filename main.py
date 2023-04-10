e0166fcd01294b0888c1e871f5e2b979ae158926
Dan Caron <dancaron@gmail.com>
2023-04-10 10:00:40 -0400

    main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+import streamlit as st
+
+
+st.title("RFdiffusion")
+st.write("Welcome to the RFdiffusion Streamlit App! Explore the functionalities of RFdiffusion for protein design tasks.")
+
+task = st.sidebar.selectbox("Select a Task", ["PPI Binder Design", "Symmetric Oligomer Design", "Symmetric Motif Scaffolding"])
+
+if task == "PPI Binder Design":
+    st.header("PPI Binder Design")
+    st.write("Design protein-protein interaction (PPI) binders using RFdiffusion.")
+    st.write("Specify parameters for binder design:")
+    chain_b = st.text_input("Chain B (target) residues [e.g. B1-100/0 100-100]:")
+    num_designs = st.number_input("Number of designs to generate:", value=10, min_value=1)
+    hotspot_res = st.text_input("Hotspot residues [e.g. A30,A33,A34]:")
+
+    # Placeholder for RFdiffusion implementation
+    if st.button("Generate Binders"):
+        st.write("Generating binders...")
+        st.write("Generated binders: [Example output]")
+
+elif task == "Symmetric Oligomer Design":
+    st.header("Symmetric Oligomer Design")
+    st.write("Design symmetric oligomers using RFdiffusion.")
+    st.write("Specify parameters for symmetric oligomer design:")
+    symmetry = st.selectbox("Symmetry type:", ["cyclic", "dihedral", "tetrahedral"])
+    contig_length = st.number_input("Total length of your oligomer:", value=360, min_value=1)
+    num_designs = st.number_input("Number of designs to generate:", value=1, min_value=1)
+
+    # Placeholder for RFdiffusion implementation
+    if st.button("Generate Symmetric Oligomers"):
+        st.write("Generating symmetric oligomers...")
+        st.write("Generated symmetric oligomers: [Example output]")
+
+elif task == "Symmetric Motif Scaffolding":
+    st.header("Symmetric Motif Scaffolding")
+    st.write("Perform symmetric motif scaffolding using RFdiffusion.")
+    st.write("Specify parameters for symmetric motif scaffolding:")
+    symmetry = st.selectbox("Symmetry type:", ["cyclic", "dihedral", "tetrahedral"])
+
+    # Placeholder for RFdiffusion implementation
+    if st.button("Perform Symmetric Motif Scaffolding"):
+        st.write("Performing symmetric motif scaffolding...")
+        st.write("Symmetric motif scaffolding result: [Example output]")
+
+st.write("Note: This app is for demonstration purposes only. The actual implementation of RFdiffusion's complex functionalities is not included here.")
+
